cc38168dcdaa9cc8eb89258d0ee4240bc575b563
cliffplaysdrums <cliffplaysdrums@gmail.com>
2020-11-10 09:13:48 -0500

    Census2019: Initial commit of Census data
    Add hardcoded 2019 Census data for convenience during initial analysis.

---
+++ src/Cenus2019.py
@@ -0,0 +1,16 @@
+"""This file contains data points found on data.census.gov using the '2019: ACS 1-Year Estimates Data Profiles'
+
+"""
+
+
+class Census2019:
+    TotalPopulation = 328239523
+    RACE = {
+        'W': 236475401,
+        'B': 41989671,
+        'A': 18636984,
+        'N': 2847336,
+        'H': 60481746
+        # PacificIslander = 628683
+        # Other = 16352553
+    }

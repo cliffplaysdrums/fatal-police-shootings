00015570786c07676a4f44affadaf5862fddcdaa
cliffplaysdrums <cliffplaysdrums@gmail.com>
2020-11-11 11:53:31 -0500

    Statistics: Return stats instead of printing
    Refactor function to return statistics instead of printing them.

--- src/Statistics.py
+++ src/Statistics.py
@@ -2,32 +2,40 @@ from src.DataLoader import ShootingsDataLoader
 from src.Cenus2019 import Census2019
 
 
-def print_race_counts(df):
+def total_shootings(df):
     """
 
     Args:
         df (pandas.DataFrame):
 
     Returns:
-
+        (tuple): counts, fraction, pop_fraction, per_million
+            counts: Total shootings of each race.
+            fraction: Each race's shootings as a fraction of the sum of counts.
+            pop_fraction: Each race's population as a fraction of the total US population.
+            per_million: Each race's shootings per million people of that race.
     """
-    print('Fatal shootings by race:')
+
+    # Get counts for each race & total (sum) since we might be missing races not in RACE.items() (e.g. 'other')
     counts = dict()
     total = 0
     for k, v in ShootingsDataLoader.RACE.items():
         counts[k] = len(df[df['race'] == v])
         total += counts[k]
 
+    # Get each race's contribution to the above total
+    fraction = dict()
     for k, v in ShootingsDataLoader.RACE.items():
-        fraction = counts[k] / total
-        print(f'\t{k}: {counts[k]} ({100 * fraction:.2f}%)')
+        fraction[k] = counts[k] / total
 
-    print('Population by race (2019):')
+    # Get each race's portion of total population from Census data
+    pop_fraction = dict()
     for k, v in Census2019.RACE.items():
-        fraction = v / Census2019.TotalPopulation
-        print(f'\t{k}: {100 * fraction:.2f}% of US population')
+        pop_fraction[k] = v / Census2019.TotalPopulation
 
-    print('Fatal shootings per million by race:')
+    # Calculate each race's total shootings per million people using Census data
+    per_million = dict()
     for k, v in Census2019.RACE.items():
-        per_million = counts[k] / (v / (1000 * 1000))
-        print(f'\t{k}: {round(per_million)} per million killed')
+        per_million[k] = counts[k] / (v / (1000 * 1000))
+
+    return counts, fraction, pop_fraction, per_million
